dd464338d8c5420c67feb863c4415be2873d72a2
FeyzanurG <117129515+FeyzanurG@users.noreply.github.com>
2023-02-22 21:50:38 -0600

    LWC with sObjects

---
+++ First Project/force-app/main/default/lwc/lwc12/lwc12.js
@@ -0,0 +1,6 @@
+import { LightningElement, wire } from 'lwc';
+import getAccountList from '@salesforce/apex/GetAccount.getAccountList';
+
+export default class Lwc12 extends LightningElement {
+    @wire(getAccountList) Account;
+}
---
+++ First Project/force-app/main/default/lwc/lwc7/lwc7.js
@@ -0,0 +1,21 @@
+import { LightningElement } from 'lwc';
+
+export default class Lwc7 extends LightningElement {
+    contactList = [
+{
+    Id:1,
+    Name:"John Doe",
+    Title:" CEO"
+},
+{
+    Id:2,
+    Name:'Jenifer Smith',
+    Title:" Senior VP"
+},
+{
+    Id:3,
+    Name:'Michael Mock',
+    Title:" VP"
+}
+    ];
+}
